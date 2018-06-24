1477456d4534e22f7c4890167ada98d8cadc3a31
Soumya Ranjan Dash <soumyardash27@gmail.com>
2018-06-24 23:49:42 +0530

    cards updated

--- GUI/js/cards.js
+++ GUI/js/cards.js
@@ -6,3 +6,18 @@ function myFunction() {
         x.style.display = "none";
     }
 }
+
+var x1;
+var y1;
+
+function doubleclick(event) {
+
+  x=event.clientX;
+  y=event.clientY;
+
+    document.getElementById("myDIV").style.top = y+"px";
+    document.getElementById("myDIV").style.left = x + "px";
+    document.getElementById("myDIV").style.display = "block";
+}
+
+document.addEventListener("click", doubleclick);
