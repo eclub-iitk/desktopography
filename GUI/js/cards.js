4b0b9ee793e44e1521f9d0cb27704d65593d631f
ashok2811 <ashok281196@gmail.com>
2018-06-25 18:35:20 +0530

    hi

--- GUI/js/cards.js
+++ GUI/js/cards.js
@@ -15,9 +15,27 @@ function doubleclick(event) {
   x=event.clientX;
   y=event.clientY;
 
+  var clientHeight = document.getElementById('myDIV').clientHeight;
+  var clientWidth = document.getElementById('myDIV').clientWidth;
+
+  if(x>(window.innerWidth-clientWidth))
+      x=window.innerWidth-clientWidth;
+  if(y>(window.innerHeight-clientHeight))
+      y=window.innerHeight-clientHeight;
+  
     document.getElementById("myDIV").style.top = y+"px";
     document.getElementById("myDIV").style.left = x + "px";
     document.getElementById("myDIV").style.display = "block";
+
+    
 }
 
 document.addEventListener("dblclick", doubleclick);
+
+function ws() {
+    var clientHeight = document.getElementById('myDIV').clientHeight;
+    var clientWidth = document.getElementById('myDIV').clientWidth;
+    
+    if(clientHeight>200){myFunction();}
+    alert(clientHeight +" "+clientWidth);
+}
